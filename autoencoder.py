aa1ceebdd52cd80b9a6ca248fb15caacf24b9f4c
abergeron <abergeron@755dec92-22f1-11de-8691-b5df825c884c>
2009-04-07 00:51:26 +0000

    - Use the new private API (unbreaks some functions). - Add a noisyness parameter.  This introduces a save file format change, but since there hasn't been much use (yet!) no attempt at backward compatibility is made

--- autoencoder.py
+++ autoencoder.py
@@ -1,33 +1,48 @@
 from simplenet import SimpleNet
 
+__all__ = ['Autoencoder', 'StackedAutoencoder']
+
 import numpy
 
+from base import *
 from nlins import *
 from errors import *
 
 class Autoencoder(SimpleNet):
-    def __init__(self, ninputs, noutputs, nlin=tanh, alpha=0.01, lmbd=0.0, dtype=numpy.float32):
-        SimpleNet.__init__(ninputs, noutputs, ninputs, hnlin=nlin, error=mse, onlin=none, alpha=alpha, lmbd=lmbd, dtype=dtype)
+    def __init__(self, ninputs, noutputs, nlin=tanh, noisyness=0.0, corrupt_value=0.0, alpha=0.01, lmbd=0.0, dtype=numpy.float32):
+        SimpleNet.__init__(self, ninputs, noutputs, ninputs, hnlin=nlin, error=mse, onlin=none, alpha=alpha, lmbd=lmbd, dtype=dtype)
+        self.noisyness = noisyness
+        self.corrupt_value = corrupt_value
 
     def _save_(self, file):
         file.write('AE1')
+        pickle.dump((self.noisyness, self.corrupt_value), file)
 
     def _load_(self, file):
         s = file.read(3)
-        if s != 'AE1':
-            raise ValueError('Not an Autoencoder save file')
+        if s == 'AE1':
+            self.noisyness, self.corrupt_value = pickle.load(file)
+        else:
+            raise ValueError('Not a valid Autoencoder save file')
 
     def test(self, x):
-        return SimpleNet.test(self, x, x)
+        return self._test(x, x)
 
     def code(self, x):
-        return self.fprop(x)[1]
-
-    def grad(self, x):
-        return SimpleNet.grad(self, x, x)
+        return self._fprop(x)[1]
 
     def epoch_bprop(self, x):
-        return SimpleNet.epoch_bprop(self, x, x)
+        return self._epoch_bprop(x, x)
+
+    def _epoch_bprop(self, x, y):
+        if self.noisyness != 0.0:
+            x[numpy.random.random(x.shape) < self.noisyness] = self.corrupt_value
+        SimpleNet._epoch_bprop(self, x, y)
 
     def train_loop(self, x, epochs=100):
-        return SimpleNet.train_loop(self, x, x, epochs=epochs)
+        return self._train_loop(x, x, epochs)
+
+
+class StackedAutoencoder(BaseObject):
+    def __init__(self, sizes, nlins, alpha=0.01, lmbd=0.0, dtype=numpy.float32):
+        self.nets = map(lambda s, n: 1, [])
