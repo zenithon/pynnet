6346eb8808461cb3a70445bd1f01279c74720321
abergeron <abergeron@755dec92-22f1-11de-8691-b5df825c884c>
2009-04-10 05:18:21 +0000

    At last, a working implementation of a stacked autoencoder.
    It appears to be working, but that was only light testing.

--- autoencoder.py
+++ autoencoder.py
@@ -1,20 +1,36 @@
-import numpy
-
 from base import *
 
 from net import *
 from simplenet import *
 from nlins import *
 from errors import *
+from trainers import *
+from trainers import Trainer
 
 __all__ = ['Autoencoder', 'StackedAutoencoder']
+
+class auto_trainer(Trainer):
+    def __init__(self, trainer_type, x, noisyness=0.0, corrupt_value=0.0, trainer_opts={}):
+        self.trainer = trainer_type(x, x, **trainer_opts)
+        self.noisyness = noisyness
+        self.corrupt_value = corrupt_value
+
+    def reset(self):
+        self.trainer.reset()
+        self.trainer.x = self.trainer.y
+
+    def epoch(self, nnet):
+        if self.noisyness > 0.0:
+            self.trainer.x = self.trainer.y.copy()
+            self.trainer.x[numpy.random.random(x.shape) < self.noisyness] = self.corrupt_value
+        self.trainer.epoch(nnet)
 
 class Autoencoder(SimpleNet):
     r"""
     Simple autoencoder with only one hidden layer.
     """
-    def __init__(self, ninputs, noutputs, nlin=tanh, noisyness=0.0, corrupt_value=0.0, alpha=0.01, lmbd=0.0, dtype=numpy.float32):
-        SimpleNet.__init__(self, ninputs, noutputs, ninputs, hnlin=nlin, error=mse, onlin=nlin, alpha=alpha, lmbd=lmbd, dtype=dtype)
+    def __init__(self, ninputs, noutputs, nlin=tanh, noisyness=0.0, corrupt_value=0.0, dtype=numpy.float32):
+        SimpleNet.__init__(self, ninputs, noutputs, ninputs, hnlin=nlin, error=mse, onlin=nlin, dtype=dtype)
         self.noisyness = noisyness
         self.corrupt_value = corrupt_value
 
@@ -37,58 +53,56 @@ class Autoencoder(SimpleNet):
         x = numpy.atleast_2d(x)
         return self._fprop(x)[1]
 
-    def epoch_bprop(self, x):
-        x = numpy.atleast_2d(x)
-        return self._epoch_bprop(x, x)
+    def train_loop(self, x, trainer_type=bprop, epochs=100, **trainer_opts):
+        return self._train_loop(auto_trainer(trainer_type, x, self.noisyness, self.corrupt_value, trainer_opts), epochs)
 
-    def _epoch_bprop(self, x, y):
-        if self.noisyness != 0.0:
-            x[numpy.random.random(x.shape) < self.noisyness] = self.corrupt_value
-        SimpleNet._epoch_bprop(self, x, y)
-
-    def train_loop(self, x, epochs=100):
-        x = numpy.atleast_2d(x)
-        return self._train_loop(x, x, epochs)
+def layer_encoder(W1, b1, nlin):
+    W2 = W1.T.copy()
+    b2 = numpy.zeros(W2.shape[1])
+    return NNet.virtual([layer(W1, b1), layer(W2, b2)], nlins=(nlin, nlin), error=mse)
 
 class StackedAutoencoder(NNet):
-    def __init__(self, sizes, nlins, noisyness=0.0, corrupt_value=0.0, alpha=0.01, lmbd=0.0, dtype=numpy.float32):
-        self.net = NNet(self, sizes + sizes[-2::-1], nlins + nlins[::-1], alpha=alpha, lmbd=lmbd, dtype=dtype)
+    def __init__(self, sizes, nlins, noisyness=0.0, corrupt_value=0.0, dtype=numpy.float32):
+        NNet.__init__(self, sizes + sizes[-2::-1], nlins + nlins[::-1], dtype=dtype)
         self.noisyness = noisyness
         self.corrupt_value = corrupt_value
 
     def _save_(self, file):
-        self.net._save_(file)
         file.write('SAE1')
         pickle.dump((self.noisyness, self.corrupt_value), file)
     
     def _load_(self, file):
-        self.net._load_(file)
         s = file.read(4)
         if s == 'SAE1':
             self.noisyness, self.corrupt_value = pickle.load(file)
         else:
-            raise ValueError('Not a valid Autoencoder save file')
+            raise ValueError('Not a valid StackedAutoencoder save file')
 
+    def code(self, x):
+        x = numpy.atleast_2d(x)
+        return self._fprop(x).outs[len(self.layers)/2-1]
+    
     def test(self, x):
         x = numpy.atleast_2d(x)
-        return self.net._test(x, x)
+        return self._test(x, x)
 
-    def pretrain(self, x):
+    def grad(self, x, lmbd):
         x = numpy.atleast_2d(x)
-        return self._pretrain(x)
+        return self._grad(x, x, lmbd)
 
-    def _pretrain(self, x):
-        ## code be here
-
-    def epoch_bprop(self, x):
+    def pretrain(self, x, trainer_type=bprop, loops=10, **trainer_opts):
         x = numpy.atleast_2d(x)
-        return self._epoch_bprop(x, x)
-
-    def _epoch_bprop(self, x, y):
-        if self.noisyness != 0.0:
-            x[numpy.random.random(x.shape) < self.noisyness] = self.corrupt_value
-        self.net._epoch_bprop(self, x, y)
-
-    def train_loop(self, x, epochs=100):
+        return self._pretrain(x, trainer_type, loops, trainer_opts)
+    
+    def _pretrain(self, x, trainer_type, loops, trainer_opts):
         x = numpy.atleast_2d(x)
-        return self.net._train_loop(x, x, epochs)
+        tnet = layer_encoder(self.layers[0].W, self.layers[0].b, nlin=self.nlins[0])
+        tnet.train_loop(auto_trainer(trainer_type, x, self.noisyness, self.corrupt_value, trainer_opts), epochs=loops)
+        
+        for i in xrange(1, len(self.layers)):
+            v = self._fprop(x).outs[i-1]
+            tnet = layer_encoder(self.layers[i].W, self.layers[i].b, nlin=self.nlins[i])
+            tnet.train_loop(auto_trainer(trainer_type, v, self.noisyness, self.corrupt_value, *trainer_opts), epochs=loops)
+    
+    def train_loop(self, x, trainer_type=bprop, epochs=100, **trainer_opts):
+        return self._train_loop(auto_trainer(trainer_type, x, self.noisyness, self.corrupt_value, trainer_opts), epochs)
