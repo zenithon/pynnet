d4533007b8ff510dca590523cc939ca0d517033e
abergeron <abergeron@755dec92-22f1-11de-8691-b5df825c884c>
2010-03-10 21:12:42 +0000

    Add softmax.

--- pynnet/nlins.py
+++ pynnet/nlins.py
@@ -1,9 +1,9 @@
 import theano.tensor as T
 
-__all__ = ['tanh', 'sigmoid', 'none']
+__all__ = ['tanh', 'sigmoid', 'softmax', 'none']
 
 from theano.tensor import tanh
-from theano.tensor.nnet import sigmoid
+from theano.tensor.nnet import sigmoid, softmax
 
 def none(x):
     return x
