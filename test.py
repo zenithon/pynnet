f569619f21634068641ff5c85d8ea37b7867b8e5
abergeron <abergeron@755dec92-22f1-11de-8691-b5df825c884c>
2010-03-10 04:29:49 +0000

    Better test script.

--- test.py
+++ test.py
@@ -1,12 +1,22 @@
-import doctest
+import doctest, sys, pkgutil, pynnet
 
-def runTests(options = doctest.ELLIPSIS or doctest.DONT_ACCEPT_TRUE_FOR_1):
-    import pynnet
-    
-    predefs = pynnet.__dict__
-    
-    for mod in [pynnet.autoencoder, pynnet.base, pynnet.errors, pynnet.net, pynnet.nlins, pynnet.simplenet, pynnet.trainers]:
-        doctest.testmod(mod, extraglobs=predefs, optionflags=options)
+def runTests(mod):
+    for (_, name, ispkg) in pkgutil.walk_packages(mod.__path__, mod.__name__+'.'):
+        if not ispkg:
+            test(name)
+
+def test(name):
+    predefs = dict(pynnet.__dict__)
+    options = doctest.ELLIPSIS or doctest.DONT_ACCEPT_TRUE_FOR_1
+    print "Testing:", name
+    __import__(name)
+    doctest.testmod(sys.modules[name], extraglobs=predefs, optionflags=options)
 
 if __name__ == '__main__':
-    runTests()
+    if len(sys.argv) > 1:
+        for mod in sys.argv[1:]:
+            if mod.endswith('.py'):
+                mod = mod[:-3]
+            test(mod)
+    else:
+        runTests(pynnet)
