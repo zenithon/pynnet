bd8c6be99cca02ef9105d72f4bf7772674557ce5
abergeron <abergeron@755dec92-22f1-11de-8691-b5df825c884c>
2010-03-22 20:32:32 +0000

    Some tests for get_updates.

--- pynnet/trainers.py
+++ pynnet/trainers.py
@@ -5,6 +5,23 @@ __all__ = ['early_stopping', 'bprop', 'eval_net', 'minibatch_eval',
            'minibatch_epoch', 'get_updates']
 
 def get_updates(params, err, alpha):
+    r"""
+    Returns a dictionary of updates suitable for the theano.function
+    
+    Tests:
+    >>> W = theano.shared(numpy.random.random((12, 8)), name='W')
+    >>> b = theano.shared(numpy.random.random((8,)), name='b')
+    >>> import theano.tensor as T
+    >>> xs = T.dmatrix('x')
+    >>> ys = T.dmatrix('y')
+    >>> x = numpy.random.random((50, 12))
+    >>> y = numpy.random.random((50, 8))
+    >>> err = T.mean((T.tanh(T.dot(xs, W)+b) - ys)**2)
+    >>> up = get_updates([W, b], err, 0.125)
+    >>> f = theano.function([xs, ys], err, updates=up)
+    >>> f(x, y) > f(x, y)
+    True
+    """
     a = theano.tensor.TensorConstant(theano.tensor.fscalar, alpha)
     gparams = theano.tensor.grad(err, params)
     return dict((p, p - gp*a) for p, gp in izip(params, gparams))
