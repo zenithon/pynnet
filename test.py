ec41e931bb2c667b5148d3797a6edf51b379e435
abergeron <abergeron@755dec92-22f1-11de-8691-b5df825c884c>
2010-04-09 23:22:58 +0000

    Improvement to test.py: it will now report undocumented methods.
    This is only done for the methods and classes defined in the module.

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
-import doctest, sys, pkgutil, pynnet
+import doctest, sys, pkgutil, types, pynnet
 
 def runTests(mod):
     for (_, name, ispkg) in pkgutil.walk_packages(mod.__path__, mod.__name__+'.'):
         if not ispkg:
             test(name)
+            cover(name)
 
 def test(name):
     predefs = dict(pynnet.__dict__)
@@ -11,6 +12,29 @@ def test(name):
     print "Testing:", name
     __import__(name)
     doctest.testmod(sys.modules[name], extraglobs=predefs, optionflags=options)
+
+def cover(name):
+    __import__(name)
+    for meth in methods_of(sys.modules[name]):
+        if meth.__name__ in ['_save_', '_load_', '__str__']:
+            continue
+        if meth.__doc__ is None:
+            print "*** No doc for:", meth.__name__
+            
+def methods_of(obj, mod=None):
+    if isinstance(obj, types.ModuleType):
+        mod = obj
+    if mod is None:
+        raise ValueError('must be called with a module')
+    for aname in dir(obj):
+        attr = getattr(obj, aname)
+        if not hasattr(attr, '__module__') or attr.__module__ != mod.__name__:
+            continue
+        if isinstance(attr, type):
+            for meth in methods_of(attr, mod):
+                yield meth
+        if isinstance(attr, (types.FunctionType, types.UnboundMethodType)):
+            yield attr
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
@@ -27,5 +51,6 @@ if __name__ == '__main__':
                 runTests(mm)
             else:
                 test(mod)
+                cover(mod)
     else:
         runTests(pynnet)
