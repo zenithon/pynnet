4515be76ec670f2ec6099731a34f41b7413ddfaa
abergeron <abergeron@755dec92-22f1-11de-8691-b5df825c884c>
2010-07-02 19:31:15 +0000

    Start of a SplitLayer (well most of it).  It does not compute shapes for now and might not for some time (since I'm lazy).  Otherwise it should work.

--- pynnet/layers/utils.py
+++ pynnet/layers/utils.py
@@ -1,4 +1,5 @@
 from base import *
+from itertools import izip
 
 # Since these layers are more or less patches or fixes for small
 # problems we do not export them.  They should only be used explicitly
@@ -42,3 +43,147 @@ class IdentityLayer(BaseLayer):
         self.output = input
         self.output_shape = input_shape
         self.params = []
+
+class iClass(object):
+    r"""
+    Helper class to pass slices to methods.
+
+    Examples:
+    >>> i[0:1]
+    slice(0, 1, None)
+    >>> i[:,3:]
+    (slice(None, None, None), slice(3, None, None))
+    """
+    def __getitem__(self, slice):
+        r"""
+        Returns the slice argument.
+        """
+        return slice
+
+i = iClass()
+
+class SplitLayerHelper(BaseLayer):
+    r"""
+    Helper class for SplitLayer.  Represents additional splits.
+    """
+    def __init__(self, name):
+        r"""
+        Tests:
+        >>> s = SplitLayerHelper('SplitLayer1-2')
+        >>> s.is_built
+        False
+        >>> s2 = test_saveload(s)
+        >>> s2.is_built
+        False
+        """
+        BaseLayer.__init__(self, name)
+        self.is_built = False
+    
+    def _save_(self, file):
+        pass
+    
+    def _load1_(self, file):
+        self.is_built = False
+    
+    _load_ = _load1_
+    
+    def build(self, input, input_shape=None):
+        r"""
+        :notests:
+        """
+        assert self.is_built, "Must build the parent SplitLayer before building dependent layers"
+
+class SplitLayer(BaseLayer):
+    r"""
+    Class to split an input expression into one or more pieces.
+    
+    The regions are specified by a list of tuples representing indexes
+    into the input. The split regions may overlap.
+
+    The first split is the output of the main class and the other will
+    be the outputs of the sublayers accessible though `helpers`.
+
+    Note: To use the convinient syntax in the examples below import
+    `i` from this module along with SplitLayer.  Otherwise you can
+    pass instances of `slice` with the appropriate parameters.
+
+    Note2: This class doesn't compute its output shape in any case.
+    (since I am lazy)  If you want to do it I will be glad.
+
+    Examples:
+    >>> s = SplitLayer([i[:,0], i[:,1:]])
+    >>> s = SplitLayer([i[:100,:], i[50:150,:], i[100:,:]])
+    >>> s = SplitLayer([i[1], i[2], i[3]])
+
+    Attributes:
+    `splits` -- (list of slices, read-only) The list of indexes for
+                each split.
+    `helpers` -- (tuple of layers, read-only) Helper layers for splits
+                 other than the first.
+    """
+    def __init__(self, splits, name=None):
+        r"""
+        Tests:
+        >>> s = SplitLayer([i[0,2], i[1:,:]])
+        >>> s.splits
+        [(0, 2), (slice(1, None, None), slice(None, None, None))]
+        >>> s2 = test_saveload(s)
+        >>> s2.splits
+        [(0, 2), (slice(1, None, None), slice(None, None, None))]
+        """
+        BaseLayer.__init__(self, name)
+        self.splits = splits
+        name = self.name
+        self.helpers = tuple(SplitLayerHelper(name+'-'+str(i+2)) for i, _ in enumerate(splits[1:]))
+
+    def _save_(self, file):
+        psave(self.splits, file)
+
+    def _load1_(self, file):
+        self.splits = pload(file)
+
+    _load_ = _load1_
+
+    def build(self, input, input_shape=None):
+        r"""
+        Build layer with input expression `input`.
+        
+        Tests:
+        >>> s = SplitLayer([i[1:3]])
+        >>> x = T.fmatrix('x')
+        >>> s.build(x)
+        >>> s.input
+        x
+        >>> s.output_shape # always None for now
+        >>> s.params
+        []
+        >>> theano.pp(s.output)
+        'x[1:3]'
+        >>> s = SplitLayer([i[:,0:2], i[:,2:]])
+        >>> s.build(x)
+        >>> s.helpers[0].input
+        x
+        >>> s.helpers[0].output_shape # also always none for now
+        >>> s.helpers[0].params
+        []
+        >>> theano.pp(s.helpers[0].output)
+        'x[:, 2:]'
+        """
+        self.input = input
+        self.params = []
+        self.output = input[self.splits[0]]
+        if input_shape is None:
+            self.output_shape = None
+            helper_shape = [None for h in self.helpers]
+        else:
+            # computing the output shape is hard and I'm lazy
+            # I'll do it later, maybe
+            self.output_shape = None
+            helper_shape = [None for h in self.helpers]
+            
+        for split, helper, shape in izip(self.splits[1:], self.helpers, helper_shape):
+            helper.input = input
+            helper.params = []
+            helper.output = input[split]
+            helper.output_shape = shape
+            helper.is_built = True
