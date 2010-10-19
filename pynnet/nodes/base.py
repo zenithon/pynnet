14ebb8682607da8333ee8db3f4209b2384fbc77d
abergeron <abergeron@755dec92-22f1-11de-8691-b5df825c884c>
2010-10-19 16:53:52 +0000

    Tests added

--- pynnet/nodes/base.py
+++ pynnet/nodes/base.py
@@ -109,11 +109,17 @@ class BaseNode(BaseObject):
         self.name = name
         self.inputs = [InputNode(input) if not isinstance(input, BaseNode)
                        else input for input in inputs]
+        self._build_dict()
+        self.local_params = []
+
+    def _build_dict(self):
+        r"""
+        Tested by __init__ and get_node.
+        """
         self._dict = dict()
         self._dict.update((l.name, l) for l in self.inputs)
         for l in self.inputs:
             self._dict.update(l._dict)
-        self.local_params = []
 
     def get_node(self, name):
         r"""
@@ -129,11 +135,27 @@ class BaseNode(BaseObject):
         return self._dict[name]
 
     def replace(self, replace_map):
+        r"""
+        Replace all instances of the specified node in the graph with
+        the new node provided.
+        
+        Tests:
+        >>> n = BaseNode([], 'n')
+        >>> n2 = BaseNode([n], 'n2')
+        >>> n2_new = n2.replace({n: BaseNode([], 'n3')})
+        >>> n2_new.inputs[0].name
+        'n3'
+        >>> n2_new.get_node('n3')
+        n3
+        >>> n2.inputs[0].name
+        'n'
+        """
         if self in replace_map:
             return replace_map[self]
         else:
             res = copy.copy(self)
             res.inputs = [i.replace(replace_map) for i in res.inputs]
+            res._build_dict()
             return res
     
     class output(prop):
@@ -175,8 +197,23 @@ class InputNode(BaseNode):
     >>> y_l = InputNode(T.ivector())
     """
     def __init__(self, expr, name=None):
+        r"""
+        >>> x = InputNode(T.fmatrix())
+        >>> x.name
+        'InputNode...'
+        >>> y = InputNode(T.fmatrix())
+        >>> x.name != y.name
+        True
+        """
         BaseNode.__init__(self, [], name)
         self.expr = expr
 
     def transform(self):
+        r"""
+        Tests:
+        
+        >>> x = InputNode(T.fmatrix('x'), 'x')
+        >>> theano.pp(x.output)
+        'x'
+        """
         return self.expr
