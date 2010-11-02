29523793c9696cd47eea9f6053452ca4b8b846b3
abergeron <abergeron@755dec92-22f1-11de-8691-b5df825c884c>
2010-11-02 21:26:16 +0000

    Use a function to build the subgraph rather than a clumsy node replace mechanism.

--- pynnet/nodes/recurrent.py
+++ pynnet/nodes/recurrent.py
@@ -36,26 +36,26 @@ class RecurrentWrapper(BaseNode):
     function.  It will work on any backend and with any shape.  You
     may have problems on the GPU (and maybe elsewhere) otherwise.
 
-    No example usage is provided as the relation between arguments is
-    rather complex.  The recommended usage is to use the provided
-    wrapper functions.  See the documentation for __init__ and
-    transform if you need to call this directly.
+    The recurrent part of the graph is built by the provided
+    `subgraph_builder` function.
 
-    This wrapper will not work for subgraph with more than one input.
+    This wrapper will not work for subgraphs with more than one input
+    or output at the moment.  There are plans to fix that in the
+    future.
 
     Attributes:
-    `subgraph` -- (node, read-only) the node upon which this one is
-                  based.
+    `subgraph_builder` -- (function, read-write) a function which
+                          builds the recursive part of the graph.
     `mem_init` -- (array_like, read-only) Initial value for the memory.
     """
-    def __init__(self, input, subgraph, subgraph_input, mem_init=None, 
+    def __init__(self, input, subgraph_builder, mem_init=None, 
                  outshp=None, dtype=theano.config.floatX, name=None):
         r"""
         Tests:
         >>> x = T.fmatrix('x')
         >>> x_n = InputNode(x)
-        >>> r = RecurrentWrapper(x, SimpleNode(x_n, 10, 5, dtype='float32'),
-        ...                      x_n, outshp=(5,), dtype='float32')
+        >>> r = RecurrentWrapper(x, lambda x_n: SimpleNode(x_n, 10, 5, dtype='float32'),
+        ...                      outshp=(5,), dtype='float32')
         >>> r.memory.value
         array([ 0.,  0.,  0.,  0.,  0.], dtype=float32)
         """
@@ -64,9 +64,8 @@ class RecurrentWrapper(BaseNode):
             mem_init = numpy.zeros(outshp, dtype=dtype)
         self.mem_init = mem_init
         self.memory = theano.shared(self.mem_init.copy(), name='memory')
-        self.subgraph = subgraph
-        self.subgraph_input = subgraph_input
-        self.local_params = self.subgraph.params
+        self.subgraph_builder = subgraph_builder
+        self.local_params = self.subgraph_builder(*self.inputs).params
 
     def clear(self):
         r"""
@@ -81,8 +80,8 @@ class RecurrentWrapper(BaseNode):
         Tests:
         >>> x = T.fmatrix('x')
         >>> x_n = InputNode(x)
-        >>> r = RecurrentWrapper(x, SimpleNode(x_n, 5, 2, dtype='float32'),
-        ...                      x_n, outshp=(2,), dtype='float32')
+        >>> r = RecurrentWrapper(x, lambda inp: SimpleNode(inp, 5, 2, dtype='float32'),
+        ...                      outshp=(2,), dtype='float32')
         >>> r.params
         [W, b]
         >>> theano.pp(r.output)
@@ -95,20 +94,17 @@ class RecurrentWrapper(BaseNode):
         (4, 2)
         >>> (r.memory.value == v[-1]).all()
         True
-        >>> x_n2 = InputNode(x)
-        >>> r=RecurrentWrapper(x, RecurrentWrapper(x_n, SimpleNode(x_n2, 6,2), x_n2, outshp=(2,)), x_n, outshp=(2,))
+        >>> r=RecurrentWrapper(x, lambda inp: RecurrentWrapper(inp, lambda inp2: SimpleNode(inp2, 6,2), outshp=(2,)), outshp=(2,))
         >>> f = theano.function([x], r.output)
         >>> v = f(numpy.random.random((3, 2)))
         >>> v.shape
         (3, 2)
         >>> (r.memory.value == v[-1]).all()
         True
-        >>> (r.subgraph.memory.value == v[-1]).all()
-        True
         """
         def f(inp, mem):
             i = InputNode(T.unbroadcast(T.shape_padleft(T.join(0,inp,mem)),0))
-            g = self.subgraph.replace({self.subgraph_input: i})
+            g = self.subgraph_builder(i)
             return g.output[0]
         
         outs,upds = theano.scan(f,sequences=[input],outputs_info=[self.memory])
