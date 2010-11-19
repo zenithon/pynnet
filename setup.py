216a173515d49657365f016759c1c6b73219785e
abergeron <abergeron@755dec92-22f1-11de-8691-b5df825c884c>
2010-11-19 15:14:00 +0000

    Bump up version since there were a lot of changes.
    Also add the new packages to the package list.

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@
 from distutils.core import setup
 
 setup(name='pynnet',
-      version='0.2.0dev',
+      version='0.3.0dev',
       description='python neural network library',
       author='Arnaud Bergeron',
       author_email='abergeron@gmail.com',
       url='http://code.google.com/p/pynnet',
-      packages=['pynnet', 'pynnet.layers'],
+      packages=['pynnet', 'pynnet.nodes', 'pynnet.dset',
+                'pynnet.dset.formats'],
       requires=['numpy', 'theano'],
       license='MIT'
       )
