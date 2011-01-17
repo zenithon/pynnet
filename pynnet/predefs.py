ffc12da32f9c0b9c92df752260cc655db7048148
abergeron <abergeron@755dec92-22f1-11de-8691-b5df825c884c>
2011-01-17 19:37:26 +0000

    This os old code that doesn't work.

--- pynnet/predefs.py
+++
@@ -1,8 +0,0 @@
-from pynnet import *
-
-def LogisticRegression(n_in, n_out):
-    r"""
-    :nodoc:
-    """
-    return NNet([Layer(n_in, n_out, activation=nlins.softmax)],
-                error=errors.nll)
