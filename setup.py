b1c6000b9ec6ad01d5384b8da24492d529a0a6eb
abergeron <abergeron@755dec92-22f1-11de-8691-b5df825c884c>
2011-03-22 05:26:00 +0000

    Use setuptools.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='pynnet',
       version='0.3.0dev',
@@ -8,8 +8,11 @@ setup(name='pynnet',
       author='Arnaud Bergeron',
       author_email='abergeron@gmail.com',
       url='http://code.google.com/p/pynnet',
-      packages=['pynnet', 'pynnet.nodes', 'pynnet.dset',
-                'pynnet.dset.formats'],
+      packages=find_packages(),
       requires=['numpy', 'theano'],
       license='MIT'
+      classifiers = [
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License"
+        ]
       )
