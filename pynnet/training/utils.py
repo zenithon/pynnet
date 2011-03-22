113d3fee935d6445272833bd7f02f7dab4cd0fbd
abergeron <abergeron@755dec92-22f1-11de-8691-b5df825c884c>
2011-03-22 22:00:23 +0000

    Revise doc and use better constant code.

--- pynnet/training/utils.py
+++ pynnet/training/utils.py
@@ -10,17 +10,16 @@ def get_updates(params, err, alpha):
     The updates are what would be done in one step of backprop over
     parameters `params` with error function `err` and step `alpha`.
 
-    A typical call of this function over a built network looks like
-    this:
+    A typical call of this function over a network looks like this:
     
-       updts = get_updates(net.params, net.cost, 0.01)
+       updts = get_updates(params, cost, 0.01)
     
     Tests:
     >>> W = theano.shared(numpy.random.random((12, 8)), name='W')
     >>> b = theano.shared(numpy.random.random((8,)), name='b')
     >>> import theano.tensor as T
-    >>> xs = T.dmatrix('x')
-    >>> ys = T.dmatrix('y')
+    >>> xs = T.matrix('x')
+    >>> ys = T.matrix('y')
     >>> x = numpy.random.random((50, 12))
     >>> y = numpy.random.random((50, 8))
     >>> err = T.mean((T.tanh(T.dot(xs, W)+b) - ys)**2)
@@ -28,7 +27,10 @@ def get_updates(params, err, alpha):
     >>> f = theano.function([xs, ys], err, updates=up)
     >>> f(x, y) > f(x, y)
     True
+    >>> up = get_updates([W, b], err, 0.05)
+    >>> W.dtype == up[W].dtype
+    True
     """
-    a = theano.tensor.TensorConstant(theano.tensor.fscalar, alpha)
+    a = theano.tensor.constant(alpha, dtype='float32')
     gparams = theano.tensor.grad(err, params)
     return dict((p, p - gp*a) for p, gp in izip(params, gparams))
