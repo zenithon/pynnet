d2df69f04981da044ebfbdf186ec622bd24e92c7
abergeron <abergeron@755dec92-22f1-11de-8691-b5df825c884c>
2011-05-17 03:45:18 +0000

    An implementation of LSTM that works!  What a novelty!
    This is a complete rewrite of the LSTM code using RecurrentNode and with proper peepholes.

--- pynnet/nodes/lstm.py
+++ pynnet/nodes/lstm.py
@@ -1,93 +1,53 @@
 from .base import *
 from pynnet.nlins import tanh, sigmoid
 from .simple import SimpleNode
-from .recurrent import RecurrentWrapper
+from .recurrent import RecurrentNode, RecurrentMemory
+from .oper import AddNode, MulNode
 
-__all__ = ['lstm_block']
+__all__ = ['LSTMNode']
 
-Broad1 = T.Rebroadcast((1, True))
+broad0 = T.Rebroadcast((0, True))
 
-class CEC(BaseNode):
+class _broad0(BaseNode):
+    def __init__(self, input):
+        BaseNode.__init__(self, [input], None)
+    transform = broad0
+
+class LSTMNode(RecurrentNode):
     r"""
-    The CEC class for LSTMs.
+    Node representing a single LSTM block.
 
-    See `lstm_block` or `lstm_block_peep` for user-friendly wrappers
-    or the code of these functions for example usage.
-
-    Inputs:
-    `map_in` -- (node, read-only) The mapping applied to the input
-                before going in the CEC.
-    `gate_in` -- (node, read-only) The gating function for input.
-    `gate_out` -- (node, read-only) The gating function for output.
-
-    Attributes:
-    `cec` -- (shared var, read-only) The memory.
-    """
-    def __init__(self, input, gate_in, gate_forget, gate_out, size, name=None,
-                 dtype=theano.config.floatX):
-        r"""
-        :nodoc:
-        """
-        BaseNode.__init__(self, [input, gate_in, gate_forget, gate_out], name)
-        self.cec = theano.shared(numpy.zeros((size,), dtype=dtype), 
-                                 name='cec')
-    
-    def clear(self):
-        r"""
-        Resets the memory to the initial value.
-        """
-        val = self.cec.value.copy()
-        val[:] = 0
-        self.cec.value = val
-
-    def transform(self, input, gate_in, gate_forget, gate_out):
-        r"""
-        Tests:
-        >>> x = T.fmatrix('x')
-        >>> g = SimpleNode(x, 3, 1, dtype='float32', nlin=sigmoid)
-        >>> c = CEC(x, g, g, g, 3, dtype='float32')
-        >>> g.params
-        [W, b]
-        >>> theano.pp(c.output)
-        '(Rebroadcast{?,1}(sigmoid(((x \\dot W) + b))) * scan(?_steps, Rebroadcast{?,1}(sigmoid(((x \\dot W) + b))), (x * Rebroadcast{?,1}(sigmoid(((x \\dot W) + b)))), cec))'
-        >>> f = theano.function([x], c.output, allow_input_downcast=True)
-        >>> r = f(numpy.random.random((4, 3)))
-        >>> r.dtype
-        dtype('float32')
-        >>> r.shape
-        (4, 3)
-        """
-        final_in = input * Broad1(gate_in)
-
-        def cecf(forget, cell_input, outp):
-            return outp * forget + cell_input
-
-        outs, upds = theano.scan(cecf,
-                                 sequences=[Broad1(gate_forget), final_in],
-                                 outputs_info=[self.cec])
-
-        for s, u in upds.iteritems():
-            s.default_update = u
-        self.cec.default_update = outs[-1]
-
-        return Broad1(gate_out) * outs
-
-def lstm_block(input, in_size, n_cells, rng=numpy.random, 
-               dtype=theano.config.floatX):
-    r"""
-    An single LSTM block with memory cells sharing gates.
+    This node includes a peephole and a forget gate.
 
     Examples:
     >>> x = T.fmatrix('x')
-    >>> l = lstm_block(x, 20, 2)
-    >>> l2 = test_saveload(l)
+    >>> l = LSTMNode(x, 20, 5, dtype='float32')
     """
-    map_in = SimpleNode(input, in_size, n_cells)
-    gate_in = SimpleNode(input, in_size, 1, nlin=sigmoid,
-                         dtype=dtype, rng=rng)
-    gate_forget = SimpleNode(input, in_size, 1, nlin=sigmoid,
-                             dtype=dtype, rng=rng)
-    gate_out = SimpleNode(input, in_size, 1, nlin=sigmoid,
-                          dtype=dtype, rng=rng)
-    block = CEC(map_in, gate_in, gate_forget, gate_out, n_cells, dtype=dtype)
-    return block
+    def __init__(self, input, in_size, n_cells, mapnlin=tanh, 
+                 rng=numpy.random, name=None, dtype=theano.config.floatX):
+        r"""
+        Tests:
+        >>> x = T.fmatrix('x')
+        >>> l = LSTMNode(x, 10, 2, dtype='float32')
+        >>> f = theano.function([x], l.output)
+        >>> r = f(numpy.random.random((8, 10)).astype('float32'))
+        >>> r.shape
+        (8, 2)
+        """
+        mem = RecurrentMemory()
+        map_in = SimpleNode(input, in_size, n_cells, nlin=mapnlin,
+                            dtype=dtype, rng=rng)
+        gate_in = _broad0(SimpleNode([input, mem], [in_size, n_cells], 1,
+                                     nlin=sigmoid, dtype=dtype, rng=rng))
+        gate_forget = _broad0(SimpleNode([input, mem], [in_size, n_cells], 1,
+                                         nlin=sigmoid, dtype=dtype, rng=rng))
+        gate_out = _broad0(SimpleNode([input, mem], [in_size, n_cells], 1,
+                                      nlin=sigmoid, dtype=dtype, rng=rng))
+        gin = MulNode(map_in, gate_in)
+        gocec = MulNode(mem, gate_forget)
+        gcec = AddNode(gin, gocec)
+        gout = MulNode(gcec, gate_out)
+        cec_init = numpy.zeros((n_cells,), dtype=dtype)
+
+        RecurrentNode.__init__(self, [input], [], mem, gout, cec_init, gcec, 
+                               name=name, nopad=True)
